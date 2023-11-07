1bdc2e4625ac81c26acb29dacdfbd9474276109d
MSenden <mario.senden@gmail.com>
2023-11-07 15:33:59 +0100

    hardcode rf parameters since model is only of V1

--- src/simulations/model.py
+++ src/simulations/model.py
@@ -1,16 +1,21 @@
 import numpy as np
+from copy import copy
 from scipy.integrate import odeint
 
 from utils import gaussian, threshold_linear, inverse_complex_log_transform, pairwise_distance
 
 class V1Model:
-    def __init__(self, model_parameters, stimulus_parameters, rf_parameters):
-        self.omega = model_parameters['omega']
-        self.decay_rate = model_parameters['lambda']
-        self.max_coupling = model_parameters['gamma']
-        self.num_populations = model_parameters['num_populations']
+    def __init__(self, parameters):
+        self.X = None
+        self.Y = None
+        self.omega = None
+        self.decay_rate = parameters['model']['lambda']
+        self.max_coupling = parameters['model']['gamma']
+        self.num_populations = parameters['model']['num_populations']
+        self.contrast_slope = parameters['model']['contrast_slope']
+        self.contrast_intercept = parameters['model']['contrast_intercept']
         
-        self._generate_receptive_fields(stimulus_parameters, rf_parameters)
+        self._generate_receptive_fields(parameters['stimulus'])
         self._generate_coupling()
         
     def update_coupling(self, coupling):
@@ -55,8 +60,8 @@ class V1Model:
         time_vector = np.arange(0, simulation_time, time_step)
         state = odeint(self._dynamics, initial_state, time_vector)
         return time_vector, state
-
-    def _generate_receptive_fields(self, stimulus_parameters, rf_parameters):
+    
+    def _generate_receptive_fields(self, stimulus_parameters):
         """
         Generate receptive fields.
 
@@ -70,36 +75,34 @@ class V1Model:
                 The radius of the stimulus.
             - side_length : float
                 The side length of the stimulus.
-        rf_parameters : array_like
-            The parameters of the receptive field function.
-            - slope : float
-                The slope of the threshold linear function.
-            - intercept : float
-                The intercept of the threshold linear function.
-            - offset : float
-                The offset of the threshold linear function.
         """
 
+        
         num_pixels = stimulus_parameters['num_pixels']
         radius = stimulus_parameters['radius']
         side_length = stimulus_parameters['side_length']
         
         lower_bound = radius - side_length / 2
         upper_bound = radius + side_length / 2
-        r = np.linspace(lower_bound, upper_bound, self.num_populations)
+        r = np.linspace(lower_bound, upper_bound, int(np.sqrt(self.num_populations)))
         X, Y = np.meshgrid(r, r)
         self.X = X.flatten()
         self.Y = Y[::-1].flatten()
+ 
+        r = np.linspace(lower_bound, upper_bound, int(np.sqrt(num_pixels)))
+        X, Y = np.meshgrid(r, r)
+        X = X.flatten()
+        Y = Y[::-1].flatten()
         
-        eccentricity = np.sqrt(X**2 + Y**2)
+        eccentricity = np.sqrt(self.X**2 + self.Y**2)
 
-        sigma = threshold_linear(eccentricity, *rf_parameters) * 0.25
+        diameter = threshold_linear(eccentricity, slope=0.172, intercept=0.25, offset=1)
+        sigma = diameter / 4
 
         self.receptive_fields = np.zeros((self.num_populations, num_pixels))
         for i in range(self.num_populations):
-            rf = gaussian(X, Y, X[i], Y[i], sigma[i])
-            self.receptive_fields[i, :] = rf / np.sum(rf)
-    
+            rf = gaussian(X, Y, self.X[i], self.Y[i], sigma[i])
+            self.receptive_fields[i, :] = rf / np.sum(rf)    
 
     def _generate_coupling(self):
         """
@@ -108,8 +111,22 @@ class V1Model:
         X_cortex, Y_cortex = inverse_complex_log_transform(self.X, self.Y)
         distances = pairwise_distance(X_cortex, Y_cortex)
         self.coupling = np.exp(-self.decay_rate * distances) * self.max_coupling
+     
+    def _compute_omega(self, stimulus):
+        """
+        Compute intrinsic frequencies based on the stimulus.
 
-        
+        Parameters
+        ----------
+        stimulus : array_like
+            The stimulus.
+        """
+        mean_luminance = np.mean(stimulus)
+        normalized_luminance = (stimulus - mean_luminance)**2 / (mean_luminance**2)
+        contrast = np.sqrt( np.matmul(self.receptive_fields, normalized_luminance) )
+        frequency = self.contrast_slope * contrast + self.contrast_intercept
+        self.omega = 2 * np.pi * frequency
+    
     def _dynamics(self, state, t):
         """
         The dynamics of the Kuramoto model.
@@ -130,6 +147,5 @@ class V1Model:
         phase_difference = theta.T - theta
         dtheta = self.omega + np.sum(self.coupling * np.sin(phase_difference), axis=1)
         return dtheta
-
 
         

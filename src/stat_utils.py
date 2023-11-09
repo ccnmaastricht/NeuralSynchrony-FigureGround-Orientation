ee153e100678dd18156adb3915ebf24fdfc9d59c
MSenden <mario.senden@gmail.com>
2023-11-09 15:29:15 +0100

    added comments and tost-p placeholder

--- src/stat_utils.py
+++ src/stat_utils.py
@@ -1,6 +1,14 @@
 from prettytable import PrettyTable
 
 def print_wald_chi_square(results):
+    """
+    Prints a table of Wald Chi-Square statistics for each variable in the model.
+
+    Parameters
+    ----------
+    results : statsmodels.regression.linear_model.RegressionResultsWrapper
+        The results of the GEE model.
+    """
     print('Wald Chi-Square:')
     table = PrettyTable()
     table.field_names = ['Variable', 'Chi-Square', 'p-value']
@@ -9,9 +17,20 @@ def print_wald_chi_square(results):
     print(table)
 
 def print_sample_info(metadata):
+    """
+    Prints information about the sample.
+
+    Parameters
+    ----------
+    metadata : pandas.DataFrame
+        The metadata of the sample.
+    """
     num_samples = metadata.shape[0]
     num_females = metadata['Sex'].value_counts()[' F']
     mean_age = metadata['Age'].mean()
     std_age = metadata['Age'].std().__round__(3)
 
     print(f'{num_samples} particpants ({num_females} female, mean age = {mean_age}, standard deviation = {std_age})')
+
+def tost_p():
+    pass

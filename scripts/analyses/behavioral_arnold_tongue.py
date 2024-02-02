8173a57e3747506f20ee2b88f2bf37fe8329a5d5
MSenden <mario.senden@gmail.com>
2024-02-02 10:59:10 +0100

    compute BAT for all sessions

--- scripts/analyses/behavioral_arnold_tongue.py
+++ scripts/analyses/behavioral_arnold_tongue.py
@@ -2,6 +2,7 @@
 This script computes the behavioral Arnold tongue of each subject as well as the average behavioral Arnold tongue.
 Results are saved in results/empirical/ and correspond to section X of the paper.
 """
+import os
 import tomllib
 import numpy as np
 from scipy.optimize import curve_fit
@@ -71,67 +72,101 @@ def create_predictors(bounds_grid_coarseness, num_grid_coarseness, bounds_contra
     return predictors
 
 
+def get_unique_counts(data):
+    """
+    Get the unique counts of the data.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+            The data.
+
+    Returns
+    -------
+    num_subjects : int
+        The number of subjects.
+    num_grid_coarseness : int
+        The number of grid coarseness values.
+    num_contrast_heterogeneity : int
+        The number of contrast heterogeneity values.
+    """
+    num_subjects = session_data['SubjectID'].nunique()
+    num_grid_coarseness = session_data['GridCoarseness'].nunique()
+    num_contrast_heterogeneity = session_data['ContrastHeterogeneity'].nunique()
+    return num_subjects, num_grid_coarseness, num_contrast_heterogeneity
+
+
+BASE_PATH = 'results/empirical/'      
+
 if __name__ == '__main__':
     # Load in silico experiment parameters
     in_silico_experiment_parameters = load_configuration()
 
     # Load data
-    data_path = 'empirical/main.csv'
+    data_path = 'data/main.csv'
     try:
         data = load_data(data_path)
     except FileNotFoundError:
         print(f"Data file not found: {data_path}")
         exit(1)
 
-    session_data = get_session_data(data, 1)
+    for session in range(1, 10):
 
-    # Get unique counts
-    num_subjects = session_data['SubjectID'].nunique()
-    num_grid_coarseness = session_data['GridCoarseness'].nunique()
-    num_contrast_heterogeneity = session_data['ContrastHeterogeneity'].nunique()
+        session_data = get_session_data(data, session)
 
-    # Initialize array for individual Arnold tongues
-    individual_arnold_tongues = np.zeros((num_subjects, num_grid_coarseness, num_contrast_heterogeneity))
-    conditions = session_data['Condition'].unique()
+        # Get unique counts
+        num_subjects, num_grid_coarseness, num_contrast_heterogeneity = get_unique_counts(session_data)
 
-    # Loop over subjects and conditions to fill individual Arnold tongues
-    for subject in range(num_subjects):
-        subject_data = get_subject_data(data, subject + 1)
-        accuracy = np.zeros(len(conditions))
-        for condition in conditions:
-            condition_data = subject_data[subject_data['Condition'] == condition]
-            accuracy[condition - 1] = condition_data['Correct'].mean()
+        # Initialize array for individual Arnold tongues
+        individual_arnold_tongues = np.zeros((num_subjects, num_grid_coarseness, num_contrast_heterogeneity))
+        conditions = session_data['Condition'].unique()
 
-        individual_arnold_tongues[subject] = accuracy.reshape((num_grid_coarseness, num_contrast_heterogeneity))
+        # Loop over subjects and conditions to fill individual Arnold tongues
+        for subject in range(num_subjects):
+            subject_data = get_subject_data(data, subject + 1)
+            accuracy = np.zeros(len(conditions))
+            for condition in conditions:
+                condition_data = subject_data[subject_data['Condition'] == condition]
+                accuracy[condition - 1] = condition_data['Correct'].mean()
 
-    # Compute average Arnold tongue
-    average_arnold_tongue = individual_arnold_tongues.mean(axis=0)
+            individual_arnold_tongues[subject] = accuracy.reshape((num_grid_coarseness, num_contrast_heterogeneity))
 
-    # Get bounds and create predictors
-    bounds_grid_coarseness = get_bounds(session_data, 'GridCoarseness')
-    bounds_contrast_heterogeneity = get_bounds(session_data, 'ContrastHeterogeneity')
-    predictors = create_predictors(bounds_grid_coarseness, num_grid_coarseness,
-                                   bounds_contrast_heterogeneity, num_contrast_heterogeneity)
+        # Compute average Arnold tongue
+        average_arnold_tongue = individual_arnold_tongues.mean(axis=0)
 
-    # Initial guesses for parameters
-    initial_params = [0.0, 0.0, 0.0]
+        # Get bounds and create predictors
+        bounds_grid_coarseness = get_bounds(session_data, 'GridCoarseness')
+        bounds_contrast_heterogeneity = get_bounds(session_data, 'ContrastHeterogeneity')
+        predictors = create_predictors(bounds_grid_coarseness, num_grid_coarseness,
+                                    bounds_contrast_heterogeneity, num_contrast_heterogeneity)
 
-    # Fit psychometric function to data
-    popt, _ = curve_fit(psychometric_function, predictors, average_arnold_tongue.flatten(), p0=initial_params)
+        # Initial guesses for parameters
+        initial_params = [0.0, 0.0, 0.0]
 
-    # Create predictors for continuous Arnold tongue
-    predictors = create_predictors(bounds_grid_coarseness, in_silico_experiment_parameters['num_grid_coarseness'],
-                                   bounds_contrast_heterogeneity, in_silico_experiment_parameters['num_contrast_heterogeneity'])
+        # Fit psychometric function to data
+        popt, _ = curve_fit(psychometric_function, predictors, average_arnold_tongue.flatten(), p0=initial_params)
 
-    # Compute continuous Arnold tongue
-    size = (in_silico_experiment_parameters['num_grid_coarseness'],
-            in_silico_experiment_parameters['num_contrast_heterogeneity'])
-    continuous_arnold_tongue = psychometric_function(predictors, *popt)
+        # Create predictors for continuous Arnold tongue
+        predictors = create_predictors(bounds_grid_coarseness, in_silico_experiment_parameters['num_grid_coarseness'],
+                                    bounds_contrast_heterogeneity, in_silico_experiment_parameters['num_contrast_heterogeneity'])
 
-    continuous_arnold_tongue = continuous_arnold_tongue.reshape(size)
+        # Compute continuous Arnold tongue
+        size = (in_silico_experiment_parameters['num_grid_coarseness'],
+                in_silico_experiment_parameters['num_contrast_heterogeneity'])
+        continuous_arnold_tongue = psychometric_function(predictors, *popt)
 
-    # Save results
-    np.save('results/empirical/optimal_psychometric_parameters.npy', popt)
-    np.save('results/empirical/average_bat.npy', average_arnold_tongue)
-    np.save('results/empirical/continuous_bat.npy', continuous_arnold_tongue)
-    np.save('results/empirical/individual_bats.npy', individual_arnold_tongues)
+        continuous_arnold_tongue = continuous_arnold_tongue.reshape(size)
+
+        # Save results
+        directory = f'{BASE_PATH}session_{session}/'
+        os.makedirs(directory, exist_ok=True)
+        
+        data_to_save = {
+            'optimal_psychometric_parameters': popt,
+            'average_bat': average_arnold_tongue,
+            'continuous_bat': continuous_arnold_tongue,
+            'individual_bats': individual_arnold_tongues
+        }
+
+        for filename, data in data_to_save.items():
+            np.save(directory + filename + '.npy', data)

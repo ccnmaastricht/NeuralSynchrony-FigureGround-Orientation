50d397c46ec7968538f590ed6cbbef6f48471946
MSenden <mario.senden@gmail.com>
2024-03-07 15:42:40 +0100

    added function to plot dAIC

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -71,11 +71,11 @@ def colored_heatmap(data,
     None
     """
 
-    figure = plt.figure(figsize=figsize,
-                        dpi=dpi if filetype != 'svg' else None)
     sns.set_style('white')
     sns.set_context('paper')
     sns.set_palette('muted')
+
+    plt.figure(figsize=figsize, dpi=dpi if filetype != 'svg' else None)
 
     title, xlabel, ylabel, cbar_label = labels
     title_fontsize, label_fontsize, tick_fontsize, cbar_labelsize = fontsizes
@@ -101,6 +101,88 @@ def colored_heatmap(data,
     plt.yticks(ytick_locations, yticks, fontsize=tick_fontsize)
 
     plt.tight_layout()
+
+    if filename is not None:
+        filename = f'{filename}.{filetype}'
+        plt.savefig(filename, dpi=dpi if filetype != 'svg' else None)
+    else:
+        plt.show()
+
+
+def plot_dAIC(session,
+              dAIC,
+              figsize,
+              labels,
+              fontsizes,
+              line_color,
+              text_color,
+              fill,
+              filename=None,
+              dpi=300,
+              filetype='svg'):
+    """
+    Plot dAIC against session with specific formatting.
+
+    Parameters
+    ----------
+    session : array_like
+        The session numbers.
+    dAIC : array_like
+        The delta AIC values.
+    figsize : tuple
+        The figure size in inches.
+    labels : tuple
+        The labels for the title, x and y axes, and the fill labels.
+    fontsizes : tuple
+        The title and label font sizes.
+    line_color : str
+        The color for the line plot.
+    text_color : str
+        The color for the text.
+    fill : tuple
+        The fill colors, alpha, x and y bounds, fill labels, and fill label
+        positions.
+    filename : str, optional
+        The filename to save the plot. The default is None.
+    dpi : int, optional
+        The resolution of the plot. The default is 300.
+    filetype : str, optional
+        The file type for the plot. The default is 'svg'.
+
+    Returns
+    -------
+    None
+    """
+    sns.set_style('whitegrid')
+    sns.set_context('paper')
+    sns.set_palette('muted')
+
+    plt.figure(figsize=figsize, dpi=dpi if filetype != 'svg' else None)
+
+    title, xlabel, ylabel = labels
+    title_fontsize, label_fontsize, tick_fontsize = fontsizes
+    fill_colors, fill_alpha, x_bounds, y_bounds, fill_labels, fill_label_x, fill_label_y = fill
+
+    plt.plot(session, dAIC, marker='o', linestyle='-', color=line_color)
+    for bounds, fill_color, label, position_x, position_y in zip(
+            x_bounds, fill_colors, fill_labels, fill_label_x, fill_label_y):
+        plt.fill_between(bounds,
+                         y_bounds[0],
+                         y_bounds[1],
+                         color=fill_color,
+                         alpha=fill_alpha)
+        plt.text(position_x,
+                 position_y,
+                 label,
+                 fontsize=label_fontsize,
+                 color=text_color)
+
+    plt.xlabel(xlabel, fontsize=label_fontsize)
+    plt.ylabel(ylabel, fontsize=label_fontsize)
+    plt.title(title, fontsize=title_fontsize)
+
+    plt.xticks(fontsize=tick_fontsize)
+    plt.yticks(fontsize=tick_fontsize)
 
     if filename is not None:
         filename = f'{filename}.{filetype}'

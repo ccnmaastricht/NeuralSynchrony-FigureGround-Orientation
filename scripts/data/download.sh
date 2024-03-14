8f482cb3fe80afc88c3b519a94e28a39d67751ba
MSenden <mario.senden@gmail.com>
2024-03-14 13:22:08 +0100

    use md5 checksum instead of sha256

--- scripts/data/download.sh
+++ scripts/data/download.sh
@@ -17,7 +17,7 @@ for file in "${files[@]}"; do
   curl -L -o "../../data/$filename" "$url"
 
   # Check the file integrity and remove if it fails
-    if [[ "$(sha256sum "../../data/$filename" | cut -d ' ' -f 1)" != "$checksum" ]]; then
+    if [[ "$(md5sum "../../data/$filename" | cut -d ' ' -f 1)" != "$checksum" ]]; then
       rm "../../data/$filename"
     fi
 
